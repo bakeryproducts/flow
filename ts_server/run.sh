38f65b93a9e10d8184f785bbc5152819621cefac
bakeryproducts <gleb.m.sokolov@gmail.com>
2021-11-07 08:44:22 +0300

    fixes

--- ts_server/run.sh
+++ ts_server/run.sh
@@ -3,4 +3,4 @@ docker exec \
     -u 0 \
     torchserve \
     bash -c \
-    "torchserve --ts-config=config.properties --start --model-store model-store --models all"
+    "torchserve --ts-config=config.properties --start --model-store model-store --models all --ncs"
